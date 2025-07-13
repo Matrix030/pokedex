34e2099e6b00a946648553cb38ea34422b453ff4
Matrix030 <rishigharat@gmail.com>
2025-07-13 19:19:36 -0400

    added testing for cleanInput logic from main.go file

---
+++ repl_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input string
+		expected []string
+	}{
+		{
+			input: "   hello world   ",
+			expected: []string{"hello", "world"},
+		},
+		{input: "Charmander Bulbasaur PIKACHU",
+		expected: []string{"charmander","bulbasaur", "pikachu"},
+	},
+}
+for _, c := range cases {
+	actual := cleanInput(c.input)
+	if len(actual) != len(c.expected) {
+		t.Errorf("Expected length %d, got %d for input %q", len(c.expected), len(actual), c.input)
+		continue
+	}
+	for i := range actual {
+		if actual[i] != c.expected[i] {
+			t.Errorf("Expected word %q, got %q at index %d for input %q", c.expected[i], actual[i], i, c.input)
+		}
+	}
+}	
+}
