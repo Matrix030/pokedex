ed7b88a5895b262a1ba4043455a481656900072d
Matrix030 <rishigharat@gmail.com>
2025-07-14 14:54:29 -0400

    added map logic to get map names from URL

---
+++ command_map.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+const baseURL = "https://pokeapi.co/api/v2/location-area/"
+
+func commandMap(cfg *config) error {
+	url := baseURL
+	if cfg.nextURL != nil {
+		url = *cfg.nextURL
+	}
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	body, err2 := io.ReadAll(resp.Body)
+	if err2 != nil {
+		return err2
+	}
+
+	var data locationAreaResponse
+	if err3 := json.Unmarshal(body, &data); err != nil {
+		return err3
+	}
+
+	for _, area := range data.Results {
+		fmt.Println(area.Name)
+	}
+
+	cfg.nextURL = data.Next
+	cfg.previousURL = data.Previous
+
+	return nil
+}
