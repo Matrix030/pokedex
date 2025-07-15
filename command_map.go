177c61d61549944f57ad1f91b483f5111fbfc097
Matrix030 <rishigharat@gmail.com>
2025-07-14 21:16:58 -0400

    merged map files

--- command_map.go
+++ command_map.go
@@ -41,3 +41,35 @@ func commandMap(cfg *config) error {
 
 	return nil
 }
+
+func commandMapB(cfg *config) error {
+	if cfg.previousURL == nil {
+		fmt.Println("You are you on the first page")
+	}
+
+	resp, err := http.Get(*cfg.previousURL)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	body, err2 := io.ReadAll(resp.Body)
+
+	if err2 != nil {
+		return err2
+	}
+	var data locationAreaResponse
+	if err := json.Unmarshal(body, &data); err != nil {
+		return err
+	}
+
+	for _, area := range data.Results {
+		fmt.Println(area.Name)
+	}
+
+	cfg.nextURL = data.Next
+	cfg.previousURL = data.Previous
+
+	return nil
+}
