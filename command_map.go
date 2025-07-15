1aed40fed08e28b9801274266a2e5867de9d2516
Matrix030 <rishigharat@gmail.com>
2025-07-15 13:10:42 -0400

    updated code to cache recently visited pages

--- command_map.go
+++ command_map.go
@@ -5,9 +5,14 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"time"
+
+	"github.com/Matrix030/pokedex/internal/pokecache"
 )
 
 const baseURL = "https://pokeapi.co/api/v2/location-area/"
+
+var cache = pokecache.NewCache(5 * time.Minute)
 
 func commandMap(cfg *config) error {
 	url := baseURL
@@ -15,6 +20,19 @@ func commandMap(cfg *config) error {
 		url = *cfg.nextURL
 	}
 
+	if cachedData, found := cache.Get(url); found {
+		fmt.Println("Using cached data!")
+
+		var data locationAreaResponse
+		if err := json.Unmarshal(cachedData, &data); err != nil {
+			return err
+		}
+		for _, area := range data.Results {
+			fmt.Println(area.Name)
+		}
+		cfg.nextURL = data.Next
+		cfg.previousURL = data.Previous
+	}
 	resp, err := http.Get(url)
 	if err != nil {
 		return err
@@ -27,15 +45,16 @@ func commandMap(cfg *config) error {
 		return err2
 	}
 
+	cache.Add(url, body)
+
 	var data locationAreaResponse
+
 	if err3 := json.Unmarshal(body, &data); err != nil {
 		return err3
 	}
-
 	for _, area := range data.Results {
 		fmt.Println(area.Name)
 	}
-
 	cfg.nextURL = data.Next
 	cfg.previousURL = data.Previous
 
@@ -44,7 +63,22 @@ func commandMap(cfg *config) error {
 
 func commandMapB(cfg *config) error {
 	if cfg.previousURL == nil {
-		fmt.Println("You are you on the first page")
+		fmt.Println("You are on the first page")
+		return nil
+	}
+
+	if cachedData, found := cache.Get(*cfg.previousURL); found {
+		var data locationAreaResponse
+		if err := json.Unmarshal(cachedData, &data); err != nil {
+			return err
+		}
+
+		for _, area := range data.Results {
+			fmt.Println(area.Name)
+		}
+		cfg.nextURL = data.Next
+		cfg.previousURL = data.Previous
+		return nil
 	}
 
 	resp, err := http.Get(*cfg.previousURL)
