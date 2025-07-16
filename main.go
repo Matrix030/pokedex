8b80e7741f9ce65a30ee47dddf24c5e01c7dc88c
Matrix030 <rishigharat@gmail.com>
2025-07-16 10:14:06 -0400

    updated config

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"time"
+
+	pokeapi "github.com/Matrix030/pokedex/internal/pokecache"
+)
+
 func main() {
-	startRepl()
+	pokeClient := pokeapi.NewClient(5 * time.Second)
+	cfg := &config{
+		pokeapiClient: pokeClient,
+	}
+
+	startRepl(cfg)
 }
