e61e22de6063f7b6d6d3512a40950931ceba0d25
Matrix030 <rishigharat@gmail.com>
2025-07-16 13:48:19 -0400

    added a print statement to check pokedex

--- command_catch.go
+++ command_catch.go
@@ -4,13 +4,9 @@ import (
 	"errors"
 	"fmt"
 	"math/rand"
-	"time"
-
-	pokeapi "github.com/Matrix030/pokedex/internal/pokeapi"
 )
 
 func commandCatch(cfg *config, args ...string) error {
-	caughtPokemon := map[string]pokeapi.Pokemon{}
 	if len(args) != 1 {
 		return errors.New("you must provide a pokemon name")
 	}
@@ -21,16 +17,17 @@ func commandCatch(cfg *config, args ...string) error {
 		return err
 	}
 
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	res := rand.Intn(pokemon.BaseExperience)
 
 	fmt.Printf("Throwing a Pokeball at %s...\n", pokemon.Name)
-
-	if r.Intn(pokemon.BaseExperience) == 0 {
-		fmt.Printf("%s was caught\n", pokemon.Name)
-		caughtPokemon[pokemon.Name] = pokemon
-	} else {
+	if res > 40 {
 		fmt.Printf("%s escaped!\n", pokemon.Name)
+		return nil
 	}
 
+	fmt.Printf("%s was caught!\n", pokemon.Name)
+	fmt.Println("You may now inspect it with the inspect command.")
+
+	cfg.caughtPokemon[pokemon.Name] = pokemon
 	return nil
 }
